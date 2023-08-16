49599b06f985047e0686c1e37fe6850d2161f3ff
diegomanga26 <diegoalexandermanga@gmail.com>
2023-08-16 15:01:50 -0500

    feat: :sparkles: Agregando endpoint.
    Se añade Endpoint que permite listar todos los productos en orden descendente.

--- routers/productos.js
+++ routers/productos.js
@@ -13,4 +13,45 @@ appProductos.get("/", limitGet(), async (req, res) => {
     res.send(result);
 });
 
+appProductos.get("/total", limitGet(), async (req, res) => {
+    if (!req.rateLimit) return;
+    let result = await productos.aggregate([
+        {
+            $lookup: {
+                from: 'inventarios',
+                localField: 'id',
+                foreignField: 'id_producto',
+                as: 'inventarios',
+            }
+        },
+        {
+            $unwind: '$inventarios',
+        },
+        {
+            $group: {
+                _id: '$_id',
+                id: { $first: '$id' },
+                nombre: { $first: '$nombre' },
+                estado: { $first: '$estado' },
+                created_by: { $first: '$created_by' },
+                total: { $sum: '$inventarios.cantidad' }
+            }
+        },
+        {
+            $sort: { total: -1 }
+        },
+        {
+            $project: {
+                _id: 0,
+                id: 1,
+                nombre: 1,
+                estado: 1,
+                created_by: 1,
+                total: 1,
+            }
+        }
+    ]).toArray();
+    res.send(result);
+});
+
 export default appProductos;
