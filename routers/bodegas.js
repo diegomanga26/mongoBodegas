6372bd90eb477d9fd12d25c599f2985bc00edfe1
diegomanga26 <diegoalexandermanga@gmail.com>
2023-08-16 14:43:44 -0500

    feat: :sparkles: Creación de Endpoint.
    Se crea Endpoint para ingreso de bodegas a la tabla bodegas.

--- routers/bodegas.js
+++ routers/bodegas.js
@@ -13,4 +13,22 @@ appBodegas.get("/", limitGet(), async (req, res) => {
     res.send(result);
 });
 
+appBodegas.post("/", limitGet(), async (req, res) => {
+
+    try {
+        let data = req.body;
+        let insert = await bodegas.insertOne(data);
+
+        if (insert.insertedId !== undefined) {
+            res.status(200).send({ msg: "Se ha guardado correctamente la Bodega." });
+        } else {
+            res.send({ status: 400, message: "Error en el ingreso de datos." });
+        }
+
+    } catch (error) {
+        res.status(400).send({ error: error });
+    }
+});
+
+
 export default appBodegas;
