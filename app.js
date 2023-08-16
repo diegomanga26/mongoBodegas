f409f2c532e2b86d841a06a8e6f08c2171b48f4e
diegomanga26 <diegoalexandermanga@gmail.com>
2023-08-15 22:16:02 -0500

    feat: :sparkles: Creacion de endpoints.
    Se crean todos los endpoints de todas las colecciones.

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-myServer = { "hostname": "127.10.10.10", "port": 5439 }
-atlasUser = "diegoalexandermanga"
-atlasPassword = "diegomanga990726"
-atlasDb = "db_campus_bodega"
+import dotenv from 'dotenv';
+import express from 'express';
+import appUsers from './routers/users.js';
+import appBodegas from './routers/bodegas.js';
+import appProductos from './routers/productos.js';
+import appInventarios from './routers/inventarios.js';
+import appHistoriales from './routers/historiales.js';
+
+dotenv.config();
+let appExpress = express();
+
+appExpress.use(express.json());
+appExpress.use("/users", appUsers);
+appExpress.use("/bodegas", appBodegas);
+appExpress.use("/productos", appProductos);
+appExpress.use("/inventarios", appInventarios);
+appExpress.use("/historiales", appHistoriales);
+
+let config = JSON.parse(process.env.myServer);
+appExpress.listen(config, ()=> {
+    console.log(`http://${config.hostname}:${config.port}`);
+});
